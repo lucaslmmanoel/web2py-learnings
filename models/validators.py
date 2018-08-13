cabda55691c6718b900bf41c366fb3dd79e925ad
lucaslmmanoel <lucaslmmanoel5@gmail.com>
2018-08-13 11:59:36 -0300

    adicionando as validações

--- models/validators.py
+++ models/validators.py
@@ -14,4 +14,6 @@ ItemsEstoque.filme.requires = IS_IN_DB(db, 'filmes.id', '%(titulo)s', _and=IS_NO
 Locacao.filmes.requires = IS_IN_DB(db, 'filmes.id', '%(titulo)s')
 Locacao.cliente.requires = IS_IN_DB(db, 'auth_user.id', '%(first_name)s %(last_name)s')
 Locacao.data_locacao.requires = IS_DATETIME(format='%d/%m/%Y')
-Locacao.data_devolucao.requires = IS_DATETIME(format='%d/%m/%Y)
+Locacao.data_devolucao.requires = IS_DATETIME(format='%d/%m/%Y')
+
+
